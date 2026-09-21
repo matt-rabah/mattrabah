688c940c8da355ff454a5972aa369274bd58a7d6
Matt Rabah <hi@cadencelab.co>
2026-09-21 03:38:34 -0400

    fix(ci): repair frontmatter workflow, disable telemetry in pre-commit, remove duplicate home, and set default tina branch

--- tina/config.ts
+++ tina/config.ts
@@ -4,7 +4,7 @@ const branch =
   process.env.GITHUB_BRANCH ||
   process.env.VERCEL_GIT_COMMIT_REF ||
   process.env.HEAD ||
-  "cms/tina-prototype";
+  "main";
 
 export default defineConfig({
   branch,
